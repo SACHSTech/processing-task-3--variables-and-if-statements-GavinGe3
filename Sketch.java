cfba060db984572ad9b59d9dd5bb741cda612289
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2022-04-11 08:41:34 -0400

    Nice

--- Sketch.java
+++ Sketch.java
@@ -15,27 +15,27 @@ public class Sketch extends PApplet {
   }
 
   // Declare time Variables 
-  int second = second();
-  int minute = minute();
-  int hour = hour();
+  int intSecond = second();
+  int intMinute = minute();
+  int intHour = hour();
 
   // Declare random location Variables
-  float pandaX = random(220, 380);
-  float pandaY = random(215, 385);
+  float fltPandaX = random(220, 380);
+  float fltPandaY = random(215, 385);
 
   // Declare time boolean for background
-  boolean nightTime = minute > 30;
+  boolean boolNightTime = intMinute > 30;
   
   public void draw() {
 
     // Set initial stroke weight  
     strokeWeight(4);
     
-    // If its half past the hour set background to dark, if not set background to light
-    if (nightTime) {
+    // If its half past the intHour set background to dark, if not set background to light
+    if (boolNightTime) {
       background(0, 0, 0);
     }
-    if (!nightTime) {
+    if (!boolNightTime) {
       background(210, 255, 173);
     }
 
@@ -64,74 +64,75 @@ public class Sketch extends PApplet {
     line(400, 510, 445, 530);
     
     // Draw panda Ears and change color based on location
-    if (pandaX >= 300 && pandaY >= 300){
+    if (fltPandaX >= 300 && fltPandaY >= 300){
       fill(140, 0, 0);
     }
-    if (pandaX <= 300 && pandaY >= 300){
+    if (fltPandaX <= 300 && fltPandaY >= 300){
       fill(0, 80, 40);
     }
-    if (pandaX >= 300 && pandaY <= 300){
+    if (fltPandaX >= 300 && fltPandaY <= 300){
       fill(227, 203, 0);
     }
-    if (pandaX <= 300 && pandaY <= 300){
+    if (fltPandaX <= 300 && fltPandaY <= 300){
       fill(242, 90, 29);
     }
-    arc(pandaX - 125, pandaY - 120, 200, 200, radians(135), radians(320), OPEN);
-    arc(pandaX + 125, pandaY - 120, 200, 200, radians(220), radians(405), OPEN);
+    arc(fltPandaX - 125, fltPandaY - 120, 200, 200, radians(135), radians(320), OPEN);
+    arc(fltPandaX + 125, fltPandaY - 120, 200, 200, radians(220), radians(405), OPEN);
 
     // Draw Panda face
     fill(255, 255, 255);
-    ellipse(pandaX, pandaY, 450, 375);
+    ellipse(fltPandaX, fltPandaY, 450, 375);
+
 
     // Draw Panda Eyes (Black Part)
-    fill(0, 0, 0);
-    arc(pandaX - 100, pandaY - 15, 150, 150, radians(149), radians(301), OPEN);
-    arc(pandaX -125, pandaY - 40, 150, 150, radians(-32), radians(122), OPEN);
-    arc(pandaX + 100, pandaY - 15, 150, 150, radians(239), radians(391), OPEN);
-    arc(pandaX + 125, pandaY - 40, 150, 150, radians(58), radians(212), OPEN);  
+    fill(0, 0, 0); 
+    arc(fltPandaX - 100, fltPandaY - 15, 150, 150, radians(149), radians(301), OPEN);
+    arc(fltPandaX -125, fltPandaY - 40, 150, 150, radians(-32), radians(122), OPEN);
+    arc(fltPandaX + 100, fltPandaY - 15, 150, 150, radians(239), radians(391), OPEN);
+    arc(fltPandaX + 125, fltPandaY - 40, 150, 150, radians(58), radians(212), OPEN);  
 
     // Draw Panda Eyes (WhitePupils)
     fill(255, 255, 255);
-    ellipse(pandaX - 90, pandaY - 48, 30, 30);
-    ellipse(pandaX + 90, pandaY - 48, 30, 30);
+    ellipse(fltPandaX - 90, fltPandaY - 48, 30, 30);
+    ellipse(fltPandaX + 90, fltPandaY - 48, 30, 30);
 
     // Draw Panda "Whiskers"
     strokeWeight(8);
-    line(pandaX, pandaY + 75, pandaX, + pandaY + 105);
+    line(fltPandaX, fltPandaY + 75, fltPandaX, + fltPandaY + 105);
     noFill();
-    arc(pandaX - 25, pandaY + 105, 50, 40, 0, radians(135));
-    arc(pandaX + 25, pandaY + 105, 50, 40, radians(45), radians(180));
+    arc(fltPandaX - 25, fltPandaY + 105, 50, 40, 0, radians(135));
+    arc(fltPandaX + 25, fltPandaY + 105, 50, 40, radians(45), radians(180));
 
     // Draw panda nose and change color based on location
-    if (pandaX < 250 || pandaX > 350){
+    if (fltPandaX < 250 || fltPandaX > 350){
       fill(26,159,228);
     }
-    if (pandaX > 251 && pandaX < 351){
+    if (fltPandaX > 251 && fltPandaX < 351){
       fill(247, 0, 0);
     }
-    ellipse(pandaX, pandaY + 75, 75, 40);
+    ellipse(fltPandaX, fltPandaY + 75, 75, 40);
 
     // Print time
     textSize(25);
-    if (hour > 9) {
-      text(hour, pandaX - 34, pandaY - 100);
+    if (intHour > 9) {
+      text(intHour, fltPandaX - 34, fltPandaY - 100);
     }
     else {
-      text("0" + hour, pandaX - 34, pandaY - 100);
+      text("0" + intHour, fltPandaX - 34, fltPandaY - 100);
     }
-    if (minute < 10){
-      text("0" + minute, pandaX, pandaY - 100);
+    if (intMinute < 10){
+      text("0" + intMinute, fltPandaX, fltPandaY - 100);
     }
     else {
-      text(minute, pandaX , pandaY - 100);
+      text(intMinute, fltPandaX , fltPandaY - 100);
     }
-    if (second < 10){
-      text("0" + second, pandaX + 35, pandaY - 100);
+    if (intSecond < 10){
+      text("0" + intSecond, fltPandaX + 35, fltPandaY - 100);
     }
     else{
-      text(second, pandaX + 35, pandaY - 100);
+      text(intSecond, fltPandaX + 35, fltPandaY - 100);
     }
-    text(":", pandaX - 6, pandaY - 100);
-    text(":", pandaX + 27, pandaY - 100);
+    text(":", fltPandaX - 6, fltPandaY - 100);
+    text(":", fltPandaX + 27, fltPandaY - 100);
   }
 }
